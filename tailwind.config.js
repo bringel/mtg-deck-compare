58a4bd98903b603ffd7fc7a50ba315abd6375c62
Brad Ringel <brad.ringel@gmail.com>
2023-03-31 19:28:12 -0400

    setting up tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    "./web/index.html",
-    "./web/src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+  content: ['./web/index.html', './web/src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: defaultTheme.colors.sky,
+        secondary: defaultTheme.colors.purple,
+        background: defaultTheme.colors.zinc,
+        common: '#1a1718',
+        uncommon: '#707883',
+        rare: '#a58e4a',
+        mythic: '#bf4427',
+        timeshifted: '#652978'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')]
 }
