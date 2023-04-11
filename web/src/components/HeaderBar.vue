01d3ba4a5bc55277f2f1885651f02d30e7569d20
Brad Ringel <brad.ringel@gmail.com>
2023-04-10 20:45:57 -0400

    adding empty app shell

---
+++ web/src/components/HeaderBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="flex items-center py-4 px-6 bg-background-800">
+    <span class="text-2xl text-primary-600">
+      MTG Deck Compare
+    </span>
+  </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
