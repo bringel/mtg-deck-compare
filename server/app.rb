4f8bcdad94382568b33c109624ab0b3807d88725
Brad Ringel <brad.ringel@gmail.com>
2023-04-23 14:30:06 -0400

    Add scryfall api service to fetch cards when DB cache misses

--- server/app.rb
+++ server/app.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 require "sinatra/base"
+require "sequel"
+require "dotenv"
+
+Dotenv.load
+
+DB = Sequel.connect(ENV["DATABASE_URL"])
+DB.extension :pg_array
+DB.extension :pg_json
+Sequel.extension :pg_array_ops
+Sequel.extension :pg_json_ops
 
 class App < Sinatra::Application
   set :public_folder, File.expand_path("#{__dir__}/../public")
