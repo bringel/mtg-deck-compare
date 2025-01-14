03a8ba5955e933cbf7ede79379b4fe08fee1b822
Brad Ringel <brad.ringel@gmail.com>
2025-01-14 17:44:56 -0500

    add api proxy and fix formatting

--- web/src/components/Button.vue
+++ web/src/components/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button :class='["rounded-sm px-4 py-2 active:border-b-0 active:top-1 relative", ...themeClasses]'>
+  <button
+    :class="['rounded-sm px-4 py-2 active:border-b-0 active:top-1 relative', ...themeClasses]"
+  >
     <slot></slot>
   </button>
 </template>
@@ -7,19 +9,18 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const props = withDefaults(defineProps<{ theme: 'primary' | 'secondary' }>(), 
-{
+const props = withDefaults(defineProps<{ theme: 'primary' | 'secondary' }>(), {
   theme: 'primary'
-})
+});
 
 const themeClasses = computed(() => {
-  switch(props.theme) {
+  switch (props.theme) {
     case 'primary':
-      return ['bg-primary-500', 'border-primary-700']
+      return ['bg-primary-500', 'border-primary-700'];
     case 'secondary':
-      return ['bg-secondary-500', 'border-secondary-700']
+      return ['bg-secondary-500', 'border-secondary-700'];
     default:
       return [];
   }
-})
+});
 </script>
