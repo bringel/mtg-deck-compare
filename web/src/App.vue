ba19c3c7e3e360b9bac49b813e092e002a494d95
Brad Ringel <brad.ringel@gmail.com>
2025-01-19 12:01:11 -0500

    have multiple deck url inputs

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,12 +1,30 @@
-<script setup lang="ts">
-import AddDeckURLInput from './components/AddDeckURLInput.vue';
-import AppShell from './components/AppShell.vue';
-</script>
-
 <template>
   <AppShell>
     <div>
-      <AddDeckURLInput />
+      <AddDeckURLInput
+        v-for="(url, index) in deckURLs"
+        :key="index"
+        :model-value="url"
+        @update:model-value="($event:string) => updateURL(index, $event)"
+        @remove="handleRemove(index)"
+      />
     </div>
   </AppShell>
 </template>
+
+<script setup lang="ts">
+import AddDeckURLInput from './components/AddDeckURLInput.vue';
+import AppShell from './components/AppShell.vue';
+import { ref } from 'vue';
+
+const deckURLs = ref(['', '']);
+
+function updateURL(index: number, value: string) {
+  deckURLs.value.splice(index, 1, value);
+}
+
+function handleRemove(index: number) {
+  deckURLs.value.splice(index, 1);
+  //test
+}
+</script>
