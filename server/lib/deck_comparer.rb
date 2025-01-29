b4fa223191480b2e71c32eb3171f48a62bf13d97
Brad Ringel <brad.ringel@gmail.com>
2025-01-28 19:17:22 -0500

    faster lookups for each deck

--- server/lib/deck_comparer.rb
+++ server/lib/deck_comparer.rb
@@ -23,15 +23,22 @@ class DeckComparer
         b_deck = b[:deck]
 
         %i[main_deck sideboard].each do |k|
+          # cards that match the card and quantity
           common[k] = a_deck[k] & b_deck[k]
+          # cards that are only in a, or cards that are in both a and b but different quantities
           a_only[k] = a_deck[k] - b_deck[k]
+          # cards that are only in b, or cards that are in both a and b but different quantities
           b_only[k] = b_deck[k] - a_deck[k]
 
+          # find the cards that are the same but with different quantities
           same_name =
             a_only[k].map { |c| c[:card] } & b_only[k].map { |c| c[:card] }
+          a_lookup = a_only[k].to_h { |c| [c.dig(:card, :name), c] }
+          b_lookup = b_only[k].to_h { |c| [c.dig(:card, :name), c] }
+
           quantities_differ[k] = same_name.map do |c|
-            a_value = a_deck[k].find { |v| v[:card] == c }
-            b_value = b_deck[k].find { |v| v[:card] == c }
+            a_value = a_lookup[c[:name]]
+            b_value = b_lookup[c[:name]]
             {
               "#{a[:index]}_quantity".to_sym => a_value[:quantity],
               "#{b[:index]}_quantity".to_sym => b_value[:quantity],
@@ -39,8 +46,8 @@ class DeckComparer
             }
           end
 
+          # remove the cards that were in both decks but with different quantities
           a_only[k] = a_only[k].reject { |c| same_name.include?(c[:card]) }
-
           b_only[k] = b_only[k].reject { |c| same_name.include?(c[:card]) }
         end
 
