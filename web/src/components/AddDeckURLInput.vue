531a6ab8a9d6b81b7c864fd703b3c4babbb3cb0e
Brad Ringel <brad.ringel@gmail.com>
2025-02-01 14:26:06 -0500

    single url input with a list of urls under

--- web/src/components/AddDeckURLInput.vue
+++ web/src/components/AddDeckURLInput.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="flex flex-grow-0">
     <input type="text" class="rounded-sm mr-4 w-96" v-model="url" />
-    <Button @click="$emit('remove')" v-if="props.removable">Remove</Button>
+    <Button @click="handleAdd">Add URL</Button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Button from './Button.vue';
-const url = defineModel<string>();
-defineEmits(['remove']);
-const props = defineProps<{ removable: boolean }>();
+const url = ref<string>();
+const emit = defineEmits<{
+  addURL: [url: string];
+}>();
+
+function handleAdd() {
+  emit('addURL', url.value);
+  url.value = '';
+}
 </script>
