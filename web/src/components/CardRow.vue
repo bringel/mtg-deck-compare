9774884681bfd60414206a8f04f795ce47bd45f3
Brad Ringel <brad.ringel@gmail.com>
2025-09-30 16:47:38 -0400

    add set icons with rarity

--- web/src/components/CardRow.vue
+++ web/src/components/CardRow.vue
@@ -3,13 +3,17 @@
   <span class="ml-2 text-white">
     {{ card.name }}
   </span>
-  <ManaCost :mana-cost="card.mana_cost" />
+  <span class="justify-self-end">
+    <ManaCost :mana-cost="card.mana_cost" />
+    <SetIcon :set="card.set_code" :rarity="card.rarity" class="text-[1.5em] ml-3" />
+  </span>
 </template>
 
 <script setup lang="ts">
 import type { Card } from '../types/Card';
 import CardQuantities from './CardQuantities.vue';
 import ManaCost from './ManaCost.vue';
+import SetIcon from './SetIcon.vue';
 
 defineProps<{ card: Card; quantities: Record<string, number> | undefined }>();
 </script>
---
+++ web/src/components/SetIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <i :class="classes"></i>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{ set: string; rarity: string }>();
+
+const classes = computed(() => {
+  return ['ss', `ss-${props.set}`, `ss-${props.rarity}`, 'ss-fw'];
+});
+</script>
