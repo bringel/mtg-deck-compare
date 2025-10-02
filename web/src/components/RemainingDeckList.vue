514d9f8021b4390554a2f47b270585e465d03d6e
Brad Ringel <brad.ringel@gmail.com>
2025-10-02 18:54:35 -0400

    trying a little style and layout tweak

--- web/src/components/RemainingDeckList.vue
+++ web/src/components/RemainingDeckList.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="grid grid-cols-[max-content_1fr_1fr] items-end gap-y-1">
-    <DeckRemainingCardRow
-      v-for="card in mainDeck?.cards"
-      :card="card"
-      :quantity="mainDeck?.quantities[card.name]"
-      :deck-index="deckIndex"
-    />
+  <div>
+    <h3 class="text-white text-lg">Deck {{ deckIndex + 1 }}</h3>
+    <div class="flex">
+      <div>
+        <h3 class="text-white text-lg">Main Deck</h3>
+        <div class="grid grid-cols-[max-content_1fr_1fr] items-end gap-y-1">
+          <DeckRemainingCardRow
+            v-for="card in mainDeck?.cards"
+            :card="card"
+            :quantity="mainDeck?.quantities[card.name]"
+            :deck-index="deckIndex"
+          />
+        </div>
+      </div>
+      <div>
+        <h3 class="text-white text-lg">Sideboard</h3>
+        <div class="grid grid-cols-[max-content_1fr_1fr] items-end gap-y-1">
+          <DeckRemainingCardRow
+            v-for="card in sideboard?.cards"
+            :card="card"
+            :quantity="sideboard?.quantities[card.name]"
+            :deck-index="deckIndex"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
