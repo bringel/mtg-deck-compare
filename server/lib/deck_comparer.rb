69873dcfe1f0a3c58d0b51ca21f50dbd4a9d4276
Brad Ringel <brad.ringel@gmail.com>
2025-10-03 16:58:57 -0400

    Update return structure of compare method to make frontend code cleaner
    The new structure has the 3 sections: common, multiple, and remaining at
    the top level with their main deck and sideboard sections underneath.
    This simplifies getting both the main and side board sections out of the
    comparison to pass to front end components
    
    Also cleanup the compare method with some named helper functions

--- server/lib/deck_comparer.rb
+++ server/lib/deck_comparer.rb
@@ -9,67 +9,72 @@ class DeckComparer
     @deck_list_urls = deck_list_urls
   end
 
-  def compare()
-    %i[main_deck sideboard].to_h do |k|
-      deck_cards = parsed_decks.map { |d| d.dig(:deck, k, :cards) }
+  def compare
+    comparison = { common: {}, multiple: {}, decks_remaining: Hash.new { {} } }
 
-      cards_in_all_decks = deck_cards.first.intersection(*deck_cards[1..-1])
-      cards_in_all_decks_quantities =
-        cards_in_all_decks.to_h do |c|
-          card_quantities =
-            parsed_decks.to_h do |d|
-              [d[:index], d.dig(:deck, k, :quantities, c.name)]
-            end
-          [c.name, card_quantities]
-        end
+    %i[main_deck sideboard].each do |section|
+      deck_cards = parsed_decks.map { |deck| deck.dig(:deck, section, :cards) }
+
+      cards_in_all_decks = find_common_cards(deck_cards)
+      cards_in_all_decks_quantities = cards_to_quantities(section: section, cards: cards_in_all_decks)
 
       decks_without_common = deck_cards.map { |deck| deck - cards_in_all_decks }
+      cards_in_multiple = find_cards_in_multiple_decks(decks_without_common)
+      cards_in_multiple_quantities = cards_to_quantities(section: section, cards: cards_in_multiple)
 
-      cards_in_more_than_one = Set.new
+      decks_remaining = build_remaining_decks_for_section(
+        section: section,
+        cards_in_all: cards_in_all_decks,
+        cards_in_multiple: cards_in_multiple
+      )
 
-      decks_without_common
-        .combination(2)
-        .each { |a, b| cards_in_more_than_one.merge(a & b) }
-      cards_in_more_than_one_quantities =
-        cards_in_more_than_one.to_h do |c|
-          card_quantities =
-            parsed_decks.to_h do |d|
-              [d[:index], d.dig(:deck, k, :quantities, c.name)]
-            end
-
-          [c.name, card_quantities.compact]
-        end
-
-      decks_remaining =
-        parsed_decks.to_h do |d|
-          cards =
-            d.dig(:deck, k, :cards) - cards_in_all_decks -
-              cards_in_more_than_one.to_a
-
-          card_names = cards.map { |c| c.name }
-          quantities =
-            d
-              .dig(:deck, k, :quantities)
-              .filter { |k, _v| card_names.include?(k) }
-          [d[:index], { cards: cards, quantities: quantities }]
-        end
-
-      v = {
-        common: {
-          cards: cards_in_all_decks,
-          quantities: cards_in_all_decks_quantities
-        },
-        multiple: {
-          cards: cards_in_more_than_one.to_a,
-          quantities: cards_in_more_than_one_quantities
-        },
-        decks_remaining: decks_remaining
+      comparison[:common][section] = {
+        cards: cards_in_all_decks,
+        quantities: cards_in_all_decks_quantities
       }
-      [k, v]
+      comparison[:multiple][section] = {
+        cards: cards_in_multiple.to_a,
+        quantities: cards_in_multiple_quantities
+      }
+
+      decks_remaining.each do |index, deck_portion|
+        comparison[:decks_remaining][index] =
+          comparison[:decks_remaining][index].merge!({ section => deck_portion })
+      end
     end
+    comparison
   end
 
   private
+
+  def find_common_cards(deck_cards)
+    deck_cards.first.intersection(*deck_cards[1..-1])
+  end
+
+  def find_cards_in_multiple_decks(decks_without_common)
+    cards_in_multiple = Set.new
+    decks_without_common.combination(2).each { |a, b| cards_in_multiple.merge(a & b) }
+    cards_in_multiple
+  end
+
+  def build_remaining_decks_for_section(section:, cards_in_all:, cards_in_multiple:)
+    parsed_decks.to_h do |deck|
+      remaining_cards = deck.dig(:deck, section, :cards) - cards_in_all - cards_in_multiple.to_a
+      card_names = remaining_cards.map(&:name)
+      quantities = deck.dig(:deck, section, :quantities).filter { |name, _| card_names.include?(name) }
+
+      [deck[:index], { cards: remaining_cards, quantities: quantities }]
+    end
+  end
+
+  def cards_to_quantities(section:, cards:)
+    cards.to_h do |card|
+      card_quantities = parsed_decks.to_h do |deck|
+        [deck[:index], deck.dig(:deck, section, :quantities, card.name)]
+      end
+      [card.name, card_quantities]
+    end
+  end
 
   def parsed_decks
     @deck_list_urls.map.with_index do |url, index|
