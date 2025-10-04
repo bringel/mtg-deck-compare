6d21ed17795ce8c61793b16e5eac21ae25289633
Brad Ringel <brad.ringel@gmail.com>
2025-10-04 18:14:35 -0400

    extract shared card row stuff

---
+++ web/src/components/CardRow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="even:bg-background-700/50 col-span-3 grid grid-cols-subgrid items-end px-2 py-1">
+    <slot name="quanties"></slot>
+    <span class="ml-2 text-white">
+      {{ card.name }}
+    </span>
+    <span class="justify-self-end">
+      <ManaCost :mana-cost="card.mana_cost" />
+      <SetIcon :set="card.set_code" :rarity="card.rarity" class="ml-3" />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Card } from '../types/Card';
+import ManaCost from './ManaCost.vue';
+import SetIcon from './SetIcon.vue';
+
+defineProps<{ card: Card }>();
+</script>
--- web/src/components/ComparisonCardRow.vue
+++ web/src/components/ComparisonCardRow.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="even:bg-background-700/50 col-span-3 grid grid-cols-subgrid items-end px-2 py-1">
-    <CardQuantities :quantities="quantities" />
-    <span class="ml-2 text-white">
-      {{ card.name }}
-    </span>
-    <span class="justify-self-end">
-      <ManaCost :mana-cost="card.mana_cost" />
-      <SetIcon :set="card.set_code" :rarity="card.rarity" class="ml-3" />
-    </span>
-  </div>
+  <CardRow :card="card">
+    <template #quanties>
+      <CardQuantities :quantities="quantities" />
+    </template>
+  </CardRow>
 </template>
 
 <script setup lang="ts">
 import type { Card } from '../types/Card';
+import CardRow from './CardRow.vue';
 import CardQuantities from './CardQuantities.vue';
-import ManaCost from './ManaCost.vue';
-import SetIcon from './SetIcon.vue';
 
 defineProps<{ card: Card; quantities: Record<string, number> | undefined }>();
 </script>
