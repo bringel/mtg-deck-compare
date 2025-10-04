dcf27413dcb6c197e3907b3be442417862c43dae
Brad Ringel <brad.ringel@gmail.com>
2025-10-04 18:14:35 -0400

    extract shared card row stuff

--- web/src/components/DeckRemainingCardRow.vue
+++ web/src/components/DeckRemainingCardRow.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="even:bg-background-700/50 col-span-3 grid grid-cols-subgrid items-end px-2 py-1">
-    <!-- there's some styling that relies on the quantity being an only child to get all the borders right-->
-    <CardQuantity :deck-index="deckIndex" :color="deckColors[deckIndex]" :quantity="quantity" />
-    <span class="ml-2 text-white">
-      {{ card.name }}
-    </span>
-    <span class="justify-self-end">
-      <ManaCost :mana-cost="card.mana_cost" />
-      <SetIcon :set="card.set_code" :rarity="card.rarity" class="ml-3 text-[1.5em]" />
-    </span>
-  </div>
+  <CardRow :card="card">
+    <template #quanties>
+      <CardQuantity :deck-index="deckIndex" :color="deckColors[deckIndex]" :quantity="quantity" />
+    </template>
+  </CardRow>
 </template>
 
 <script setup lang="ts">
 import { deckColors } from '../lib/deckColors';
 import type { Card } from '../types/Card';
 import CardQuantity from './CardQuantity.vue';
-import ManaCost from './ManaCost.vue';
-import SetIcon from './SetIcon.vue';
+import CardRow from './CardRow.vue';
 
 defineProps<{ card: Card; deckIndex: number; quantity: number | undefined }>();
 </script>
