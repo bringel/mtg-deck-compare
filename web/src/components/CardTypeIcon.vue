b2404e18e572fd774e831037093e71807212c354
Brad Ringel <brad.ringel@gmail.com>
2025-10-05 11:07:05 -0400

    card type sorting in other sections

---
+++ web/src/components/CardTypeIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <i :class="['ms', `ms-${cardType}`, 'ms-fw', 'mr-1']"></i>
+</template>
+
+<script setup lang="ts">
+import { type CardType } from '../types/Card';
+
+defineProps<{ cardType: CardType }>();
+</script>
