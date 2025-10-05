a8b35d158057b90c9837fd14fc85f109e77c2345
Brad Ringel <brad.ringel@gmail.com>
2025-10-05 10:24:26 -0400

    Sort cards in list by type

--- web/src/components/ComparisonSection.vue
+++ web/src/components/ComparisonSection.vue
@@ -3,11 +3,18 @@
     <div class="flex-grow">
       <h3 class="text-lg text-white">Main Deck</h3>
       <div class="grid grid-cols-[max-content_1fr_max-content] items-end">
-        <ComparisonCardRow
-          v-for="card in section?.main_deck.cards"
-          :card="card"
-          :quantities="section?.main_deck.quantities[card.name]"
-        />
+        <template v-for="cardType in cardTypes">
+          <template v-if="sortedMainDeck[cardType].length > 0">
+            <div class="col-span-3 flex items-center py-1 text-base">
+              <i :class="['ms', `ms-${cardType}`, 'ms-fw', 'mr-1']"></i>{{ capitalize(cardType) }}
+            </div>
+            <ComparisonCardRow
+              v-for="card in sortedMainDeck[cardType]"
+              :card="card"
+              :quantities="section?.main_deck.quantities[card.name]"
+            />
+          </template>
+        </template>
       </div>
     </div>
     <div class="flex-grow">
@@ -24,10 +31,21 @@
 </template>
 
 <script setup lang="ts">
-import type { ComparisonDeckSection } from 'src/types/ComparisonDeckSection';
+import type { ComparisonDeckSection } from '../types/ComparisonDeckSection';
 import ComparisonCardRow from './ComparisonCardRow.vue';
+import { groupByCardTypes } from '../lib/cardTypeSorter';
+import { computed } from 'vue';
+import { cardTypes } from '../types/Card';
 
-defineProps<{
+const props = defineProps<{
   section: { main_deck: ComparisonDeckSection; sideboard: ComparisonDeckSection } | undefined;
 }>();
+
+const sortedMainDeck = computed(() => {
+  return groupByCardTypes(props.section?.main_deck.cards ?? []);
+});
+
+function capitalize(str: String) {
+  return `${str.charAt(0).toLocaleUpperCase()}${str.slice(1)}`;
+}
 </script>
--- web/src/lib/deckColors.ts
+++ web/src/lib/deckColors.ts
@@ -1,3 +1,3 @@
-export type DeckColor = 'orange' | 'cyan' | 'violet' | 'pink' | 'white';
+export const deckColors = ['orange', 'cyan', 'violet', 'pink', 'white'] as const;
 
-export const deckColors: Array<DeckColor> = ['orange', 'cyan', 'violet', 'pink', 'white'];
+export type DeckColor = (typeof deckColors)[number];
