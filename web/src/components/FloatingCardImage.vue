296ed687acc9744aca8a561958186d8b986cc313
Brad Ringel <brad.ringel@gmail.com>
2025-10-07 18:19:14 -0400

    add hovering card when mousing over rows

---
+++ web/src/components/FloatingCardImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <figure class="z-10 hidden group-hover:flex">
+    <img
+      v-for="imageURL in cardImageUrls"
+      :src="imageURL"
+      class="w-96 rounded-[4.75%_/_3.5%] shadow-md shadow-black"
+    />
+  </figure>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ cardImageUrls: Array<string> }>();
+</script>
