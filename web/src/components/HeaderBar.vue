ac5b35478d729e219b395b054a69595b8d031088
Brad Ringel <brad.ringel@gmail.com>
2025-10-10 17:18:33 -0400

    add a theme toggle to the header bar

--- web/src/components/HeaderBar.vue
+++ web/src/components/HeaderBar.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav class="bg-background-800 flex items-center px-6 py-4">
+  <nav class="bg-background-800 flex items-center justify-between px-6 py-4">
     <span class="text-primary-600 text-2xl"> MTG Deck Compare </span>
+    <ThemeToggle />
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ThemeToggle from './ThemeToggle.vue';
+</script>
 
 <style scoped></style>
