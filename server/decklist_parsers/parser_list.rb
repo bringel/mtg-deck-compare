8ab1365d701ebbbe4b56809f7580afed9e7f9235
Brad Ringel <brad.ringel@gmail.com>
2025-10-23 17:18:39 -0400

    add deckstats.net parser

--- server/decklist_parsers/parser_list.rb
+++ server/decklist_parsers/parser_list.rb
@@ -3,10 +3,16 @@
 require_relative "./aetherhub_parser.rb"
 require_relative "./moxfield_parser"
 require_relative "./archidekt_parser"
+require_relative "./deckstats_parser"
 
 module DecklistParsers
   class ParserList
-    PARSERS = [AetherhubParser, MoxfieldParser, ArchidektParser]
+    PARSERS = [
+      AetherhubParser,
+      MoxfieldParser,
+      ArchidektParser,
+      DeckstatsParser
+    ]
 
     def self.get_parser(url)
       PARSERS.find { |parser| parser.can_handle_url?(url) }
