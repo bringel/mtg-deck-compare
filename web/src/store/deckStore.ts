9d374d957f41946421cd95e9b75e83c8f1a755b0
Brad Ringel <brad.ringel@gmail.com>
2026-01-02 09:52:05 -0500

    move storage of deck URL list to query string which allows for sharable links

--- web/src/store/deckStore.ts
+++ web/src/store/deckStore.ts
@@ -1,17 +1,33 @@
 import { useFetch, type UseFetchReturn } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import { reactive, ref, type Reactive } from 'vue';
+import { reactive, type Reactive } from 'vue';
 import { type Deck } from '../types/Deck';
+import { computed } from 'vue';
 
 export const useDeckStore = defineStore('decks', () => {
-  const deckURLs = ref<Array<string>>([]);
-
   const deckFetchers = reactive(new Map<string, Reactive<UseFetchReturn<Deck>>>());
+  const deckURLs = computed(() => {
+    return Array.from(deckFetchers.keys());
+  });
+
+  function updateDecks(urls: string[]) {
+    const currentURLs = Array.from(deckFetchers.keys());
+
+    currentURLs.forEach((url) => {
+      if (!urls.includes(url)) {
+        deckFetchers.delete(url);
+      }
+    });
+
+    urls.forEach((url) => {
+      if (!currentURLs.includes(url)) {
+        loadDeck(url);
+      }
+    });
+  }
 
   function loadDeck(url: string) {
     if (!deckFetchers.has(url)) {
-      deckURLs.value = [...deckURLs.value, url];
-
       const apiURL = `/api/load_deck?url=${url}`;
       const fetcher = useFetch(apiURL).json<Deck>();
       deckFetchers.set(url, reactive(fetcher));
@@ -19,13 +35,14 @@ export const useDeckStore = defineStore('decks', () => {
   }
 
   function removeDeck(url: string) {
-    deckURLs.value = deckURLs.value.filter((v) => v !== url);
+    deckFetchers.delete(url);
   }
 
   return {
     deckURLs,
     deckFetchers,
     loadDeck,
-    removeDeck
+    removeDeck,
+    updateDecks
   };
 });
